709d726ecc59eb9574eb298ad32ce44d97aed0dd
MohamedDev <kouch5568@gmail.com>
2026-02-03 05:20:49 +0100

    Create variables_and_types.py

---
+++ Basic-Python/variables_and_types.py
@@ -0,0 +1,16 @@
+# Variables and Data Types
+
+age = 20
+height = 1.75
+name = "Mohamed"
+is_student = True
+
+print(age)
+print(height)
+print(name)
+print(is_student)
+
+print(type(age))
+print(type(height))
+print(type(name))
+print(type(is_student))
